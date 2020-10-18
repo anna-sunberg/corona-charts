1c92cee4e01b38c0822c76fa186e99a2662229fb
Anna Osipova <anna.osipova@smartly.io>
2020-10-18 10:15:13 +0300

    Add country selection

--- src/App.js
+++ src/App.js
@@ -1,33 +1,72 @@
 import React, { useEffect, useState } from 'react';
 import CaseDeathsChart from './CaseDeathsChart';
 import TrendLineChart from './TrendLineChart';
+import CountrySelector from './CountrySelector';
 import { ResizableBox } from 'react-resizable';
 import { compareAsc, differenceInDays, format, parse } from 'date-fns';
+import { useLocalStorage, writeStorage } from '@rehooks/local-storage';
 import './styles.css';
 import 'react-resizable/css/styles.css';
 
 export default function App() {
-  const country = 'finland';
+  const [initialCountries] = useLocalStorage('favoriteCountries', []);
+  const [initialCountry] = useLocalStorage('favoriteCountry', 'Finland');
+  const [country, setCountry] = useState(initialCountry);
+  const [countries, setCountries] = useState(initialCountries);
   const days = differenceInDays(new Date(), new Date(2020, 2, 1));
   const [data, setData] = useState([]);
   const [countryData, setCountryData] = useState(null);
+
+  const removeFavoriteCountry = (countryToRemove) => {
+    const newCountries = countries.filter((c) => c !== countryToRemove);
+    if (!newCountries.find((c) => c === country)) {
+      setCountry(newCountries[0]);
+    }
+    setCountries(newCountries);
+  };
+
+  useEffect(() => {
+    writeStorage('favoriteCountries', countries);
+  }, [countries]);
+
+  useEffect(() => {
+    writeStorage('favoriteCountry', country);
+  }, [country]);
 
   useEffect(() => {
     async function fetchCountryData() {
       const response = await fetch(
         `https://disease.sh/v3/covid-19/countries/${country}?strict=true&allowNull=true`
       );
+      if (response.status !== 200) {
+        setCountryData(null);
+        return;
+      }
       const json = await response.json();
+
       setCountryData(json);
     }
     fetchCountryData();
   }, [country, days]);
 
   useEffect(() => {
+    if (!countryData) {
+      return;
+    }
+    if (!countries.find((c) => c === countryData.country)) {
+      setCountries([...countries, countryData.country]);
+    }
+  }, [countryData]);
+
+  useEffect(() => {
     async function fetchData() {
       const response = await fetch(
         `https://disease.sh/v3/covid-19/historical/${country}?lastdays=${days}`
       );
+      if (response.status !== 200) {
+        setData([]);
+        return;
+      }
       const json = await response.json();
       const newData = [];
 
@@ -48,8 +87,16 @@ export default function App() {
 
   return (
     <div className="App" style={{ paddingRight: '20px' }}>
-      Latest data:
-      {countryData && ` ${format(new Date(countryData.updated), 'dd.MM.yy HH:mm')}`}
+      <div className="top-bar">
+        Latest data:
+        {countryData && ` ${format(new Date(countryData.updated), 'dd.MM.yy HH:mm')}`}
+        <CountrySelector
+          country={(countryData && countryData.country) || country}
+          countries={countries}
+          selectCountry={setCountry}
+          removeCountry={removeFavoriteCountry}
+        />
+      </div>
       <ResizableBox height={400}>
         <div style={{ width: '100%', height: '100%' }}>
           {data && countryData && (
---
+++ src/CountrySelector.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import classnames from 'classnames';
+
+export default ({ selectCountry, removeCountry, country, countries }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSelect(inputValue);
+    }
+  };
+
+  const handleSelect = (value) => {
+    selectCountry(value);
+    setInputValue('');
+  };
+
+  const handleRemoveClick = (e, availableCountry) => {
+    e.stopPropagation();
+    removeCountry(availableCountry);
+  };
+
+  return (
+    <div className="flex">
+      <div>
+        Type country name:{' '}
+        <input
+          name="country"
+          value={inputValue}
+          onBlur={() => handleSelect(inputValue)}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </div>
+      {countries.map((availableCountry) => (
+        <div
+          className={classnames('country', { selected: availableCountry === country })}
+          onClick={() => handleSelect(availableCountry)}
+        >
+          {availableCountry} <span onClick={(e) => handleRemoveClick(e, availableCountry)}>×</span>
+        </div>
+      ))}
+    </div>
+  );
+};
